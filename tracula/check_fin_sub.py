0f8797d73388c980f3a3f235a2a23ee4f1d372ad
fliem <franz.liem>
2016-11-15 16:59:05 +0100

    fixes

--- tracula/check_fin_sub.py
+++ tracula/check_fin_sub.py
@@ -1,15 +1,17 @@
-form glob import glob
+from glob import glob
 import os
-
 in_data = "/data/tracula_in_data/sourcedata"
 out_data = "/data/tracula_out_data"
-
 os.chdir(in_data)
 
 sub_list = glob("sub*")
 failed_sub = []
+ok_sub = []
 
 for s in sub_list:
     if not os.path.exists(os.path.join(out_data, "run_"+ s)):
         failed_sub.append(s)
+    else:
+        ok_sub.append(s)
+
 print(failed_sub)
