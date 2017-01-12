86dc6f581f4df2b12d555f11f553a2b83ac57128
fliem <franz.liem>
2017-01-12 13:38:33 +0100

    ud

--- scripts/nii_conversion/science_cloud/run_test_conversion.sh
+++ scripts/nii_conversion/science_cloud/run_test_conversion.sh
@@ -1,6 +1,5 @@
-
 python /home/ubuntu/gtrac_long_repo/gc3pie/gc3apps/inapic/bidsapps.py \
-"--volume /data.nfs/code/:/code fliem/lhab_docker:v0.8.8.dev.nolhpipe python /code/lhab_pipelines/scripts/nii_conversion/run_nii_conversion.py" /data.nfs/bidsapps_test/01_RAW /data.nfs/bidsapps_test/ participant \
--pf /data.nfs/bidsapps_test/01_RAW/00_PRIVATE_sub_lists/lhab_10subj.tsv \
+"--volume /data.nfs/code/:/code fliem/lhab_docker:v0.8.8.dev.nolhpipe python /code/lhab_pipelines/scripts/nii_conversion/run_nii_conversion.py" /data.nfs/LHAB/01_RAW /data.nfs/LHAB/ participant \
+-pf /data.nfs/LHAB/01_RAW/00_PRIVATE_sub_lists/lhab_13subj.tsv \
 -ra "--no-public_output " \
--vvv -s testbids.conv -o /data.nfs/bidsapps_test/logfiles -N -C 15 -c 2
+-s testbids.conv -o /data.nfs/LHAB/logfiles -C 15 -c 2 -N
