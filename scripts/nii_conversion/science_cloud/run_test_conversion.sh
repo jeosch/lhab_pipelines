ab5441c77386124e81879104a9bbf5d3f2dd82c1
fliem <franz.liem@gmail.com>
2017-01-17 14:57:14 +0100

    added ds version

--- scripts/nii_conversion/science_cloud/run_test_conversion.sh
+++ scripts/nii_conversion/science_cloud/run_test_conversion.sh
@@ -1,4 +1,4 @@
-python /home/ubuntu/gtrac_long_repo/gc3pie/gc3apps/inapic/bidsapps.py \
+python /data.nfs/code/bidswrapps/bidswrapps.py \
 "--volume /data.nfs/code/:/code fliem/lhab_docker:v0.8.8.dev.nolhpipe python /code/lhab_pipelines/scripts/nii_conversion/run_nii_conversion.py" /data.nfs/LHAB/01_RAW /data.nfs/LHAB/ participant \
 -pf /data.nfs/LHAB/01_RAW/00_PRIVATE_sub_lists/lhab_13subj.tsv \
 -ra "--no-public_output " \
