a8cefee9dc9f0e5c065a3c7d61b9a32aabcbda64
fliem <franz.liem@gmail.com>
2017-01-18 17:04:10 +0100

    ud

--- scripts/nii_conversion/science_cloud/run_test_conversion.sh
+++ scripts/nii_conversion/science_cloud/run_test_conversion.sh
@@ -1,5 +1,18 @@
 python /data.nfs/code/bidswrapps/bidswrapps.py \
-"--volume /data.nfs/code/:/code fliem/lhab_docker:v0.8.8.dev.nolhpipe python /code/lhab_pipelines/scripts/nii_conversion/run_nii_conversion.py" /data.nfs/LHAB/01_RAW /data.nfs/LHAB/ participant \
+fliem/lhab_docker:v0.8.8.dev.nolhpipe \
+/data.nfs/LHAB/01_RAW /data.nfs/LHAB/NIFTI participant \
+--volume /data.nfs/code/:/code \
 -pf /data.nfs/LHAB/01_RAW/00_PRIVATE_sub_lists/lhab_13subj.tsv \
--ra "--no-public_output " \
--s testbids.conv -o /data.nfs/LHAB/logfiles -C 15 -c 2 -N
+--runscript_cmd "python /code/lhab_pipelines/scripts/nii_conversion/run_nii_conversion.py" \
+-ra "--no-public_output --ds_version 1.0.0.dev " \
+-s testbids.conv.private -o /data.nfs/LHAB/logfiles/logs_private -C 15 -c 2 -N
+
+
+python /data.nfs/code/bidswrapps/bidswrapps.py \
+fliem/lhab_docker:v0.8.8.dev.nolhpipe \
+/data.nfs/LHAB/01_RAW /data.nfs/LHAB/NIFTI participant \
+--volume /data.nfs/code/:/code \
+-pf /data.nfs/LHAB/01_RAW/00_PRIVATE_sub_lists/lhab_13subj.tsv \
+--runscript_cmd "python /code/lhab_pipelines/scripts/nii_conversion/run_nii_conversion.py" \
+-ra "--ds_version 1.0.0.dev " \
+-s testbids.conv.public -o /data.nfs/LHAB/logfiles/logs_public -C 15 -c 2 -N
