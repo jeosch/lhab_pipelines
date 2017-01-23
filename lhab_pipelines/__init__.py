4eceddefcf685927c4a567edba1174b5fc6a7ffd
fliem <franz.liem@gmail.com>
2017-01-23 17:13:26 +0100

    read version from file

--- lhab_pipelines/__init__.py
+++ lhab_pipelines/__init__.py
@@ -1,9 +1,11 @@
 from .utils import check_docker_container_version
+import os
 
 __package__ = "lhab_pipelines"
 __all__ = ["nii_conversion"]
 
-__version__ = "v0.8.dev"
-
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(dir_path, "../version")) as fi:
+    __version__ = fi.read().strip()
 
 check_docker_container_version("fliem/lhab_docker:v0.7")
