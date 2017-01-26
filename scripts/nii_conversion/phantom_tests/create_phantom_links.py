9cf127f6b01f461edae82bff1076e7849a9de1b2
fliem <franz.liem@gmail.com>
2017-01-26 12:56:07 +0100

    added dwi conversion ci tests

--- scripts/nii_conversion/phantom_tests/create_phantom_links.py
+++ scripts/nii_conversion/phantom_tests/create_phantom_links.py
@@ -1,23 +1,22 @@
 import os, shutil
 
-phantom_base = "phantom_varscale"
-
 if os.path.isdir("01_RAW"):
     shutil.rmtree("01_RAW")
 os.makedirs("01_RAW")
 
 tps = ["T1", "T3"]
 id = "phan"
-mapping = {"*resting2000*": "lhab_{sub}_fmri_{tp}",
-           "*resting_pa*": "lhab_{sub}_fmri_pa_{tp}",
-           # "*dti_high*": "lhab_{sub}_dti_{tp}",
-           # "*dti_nodif_ap*": "lhab_{sub}_dti_ap_{tp}",
-           # "*dti_nodif_pa*": "lhab_{sub}_dti_pa_{tp}",
-           "*b0map*": "lhab_{sub}_dti_b0_{tp}",
-           "*flair_long*": "lhab_{sub}_2dflair_{tp}",
-           "*3d_brain_view*": "lhab_{sub}_3dflair_{tp}",
-           "*t2w*": "lhab_{sub}_t2w_{tp}",
-           "*t1w*": "lhab_{sub}_t1w_{run}_{tp}"}
+mapping = {"lhab_{sub}_fmri_{tp}": "phantom_varscale",
+           "lhab_{sub}_fmri_pa_{tp}": "phantom_varscale",
+           "lhab_{sub}_dti_b0_{tp}": "phantom_varscale",
+           "lhab_{sub}_2dflair_{tp}": "phantom_varscale",
+           "lhab_{sub}_3dflair_{tp}": "phantom_varscale",
+           "lhab_{sub}_t2w_{tp}": "phantom_varscale",
+           "lhab_{sub}_t1w_{run}_{tp}": "phantom_varscale",
+           "lhab_{sub}_dti_{tp}": "ph_26012017_1153144_4_1_dtihighisoeV4",
+           "lhab_{sub}_dti_ap_{tp}": "ph_26012017_1151068_2_1_dtinodifapV4",
+           "lhab_{sub}_dti_pa_{tp}": "ph_26012017_1152106_3_1_dtinodifpaV4",
+           }
 
 slist_dir = os.path.join("01_RAW", "00_PRIVATE_sub_lists")
 os.makedirs(slist_dir)
@@ -36,6 +35,9 @@ os.chdir(output_dir)
 print(os.path.relpath(os.path.join(base_dir, "00_face")), "00_face")
 os.symlink(os.path.relpath(os.path.join(base_dir, "00_face")), "00_face")
 
+data_dir = os.path.join(base_dir, "data")
+
+print(mapping)
 for tp in tps:
     output_dir = os.path.join(base_dir, "01_RAW", tp, "01_noIF", "lhab_{sub}_{tp}_raw".format(sub=id, tp=tp.lower()))
 
@@ -43,9 +45,11 @@ for tp in tps:
         os.makedirs(output_dir)
     os.chdir(output_dir)
 
-    for seq in mapping.values():
-        print(seq)
+    for seq, phantom_base in mapping.items():
+        print(phantom_base, seq)
         for ext in [".par", ".rec"]:
-            # print(phantom_base + ext, os.path.join(output_dir, seq.format(sub=id, tp=tp.lower(), run=run) + ext))
-            os.symlink(os.path.relpath(os.path.join(base_dir, phantom_base + ext)),
+            os.symlink(os.path.relpath(os.path.join(data_dir, phantom_base + ext)),
                        seq.format(sub=id, tp=tp, run=run) + ext)
+            print(os.path.relpath(os.path.join(data_dir, phantom_base + ext)),
+                  seq.format(sub=id, tp=tp, run=run) + ext)
+    print("")
