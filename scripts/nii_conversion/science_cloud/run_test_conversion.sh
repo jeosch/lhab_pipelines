766b7343a8abc5311d7c78a71e03c897b70d6886
fliem <franz.liem@gmail.com>
2017-01-27 06:13:34 +0100

    ud

--- scripts/nii_conversion/science_cloud/run_test_conversion.sh
+++ scripts/nii_conversion/science_cloud/run_test_conversion.sh
@@ -1,3 +1,4 @@
+# MRIDEFACE
 python /data.nfs/code/bidswrapps/bidswrapps.py \
 fliem/lhab_pipelines:v1.0.0.dev11 \
 /data.nfs/LHAB/01_RAW /data.nfs/LHAB/NIFTI participant \
@@ -14,6 +15,27 @@ fliem/lhab_pipelines:v1.0.0.dev11 \
 --runscript_cmd "python /code/lhab_pipelines/scripts/nii_conversion/run_nii_conversion.py" \
 -ra "--ds_version 1.0.0.dev11" \
 -s lhab.conv.public.v1.0.0.dev11 -o /data.nfs/LHAB/logfiles/v1.0.0.dev11/logs_public -C 15 -c 2 -N
+
+
+
+# PYDEFACE
+python /data.nfs/code/bidswrapps/bidswrapps.py \
+fliem/lhab_pipelines:v1.0.0.dev.pydeface.2 \
+/data.nfs/LHAB/01_RAW /data.nfs/LHAB/NIFTI participant \
+-pf /data.nfs/LHAB/01_RAW/00_PRIVATE_sub_lists/lhab_13subj.tsv \
+--runscript_cmd "python /code/lhab_pipelines/scripts/nii_conversion/run_nii_conversion.py" \
+-ra "--no-public_output --ds_version v1.0.0.dev.pydeface.2" \
+-s lhab.conv.private.v1.0.0.dev.pydeface.2 -o /data.nfs/LHAB/logfiles/v1.0.0.dev.pydeface.2/logs_private -C 15 -c 2 -N
+
+
+python /data.nfs/code/bidswrapps/bidswrapps.py \
+fliem/lhab_pipelines:v1.0.0.dev.pydeface.2 \
+/data.nfs/LHAB/01_RAW /data.nfs/LHAB/NIFTI participant \
+-pf /data.nfs/LHAB/01_RAW/00_PRIVATE_sub_lists/lhab_13subj.tsv \
+--runscript_cmd "python /code/lhab_pipelines/scripts/nii_conversion/run_nii_conversion.py" \
+-ra "--ds_version v1.0.0.dev.pydeface.2" \
+-s lhab.conv.public.v1.0.0.dev.pydeface.2 -o /data.nfs/LHAB/logfiles/v1.0.0.dev.pydeface.2/logs_public -C 15 -c 2 -N
+
 
 
 # FAST
