067ffc4b07038aab89b3868e2d2b17d0c24fa171
fliem <franz.liem@gmail.com>
2017-01-31 12:03:41 +0100

    upd

--- scripts/nii_conversion/run_calc_demos.py
+++ scripts/nii_conversion/run_calc_demos.py
@@ -18,6 +18,8 @@ import os
 from lhab_pipelines.utils import read_tsv
 from lhab_pipelines.nii_conversion.conversion import calc_demos
 import argparse
+import getpass
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="creates age, sex tables.")
@@ -26,11 +28,8 @@ if __name__ == "__main__":
     parser.add_argument('output_base_dir', help='The directory where the output files '
                                                 'should be stored.'
                                                 '\n original: output_dir')
-    parser.add_argument('analysis_level', help='Level of the analysis that will be performed. ',
-                        choices=['group'])
-    parser.add_argument('--participant_label', help='The label of the participant that should be analyzed.'
-                                                    'For the conversion wf this should be given as lhab_1234',
-                        nargs="+")
+    parser.add_argument('--participant_file', help='participants that should be analyzed.'
+                                                   'For the conversion wf this should be given as lhab_1234')
     parser.add_argument('--no-public_output',
                         help="Don't create public output.\nIf public_output: strips all info about original "
                              "subject_id, file, date \nDefault: use public_output",
@@ -39,7 +38,7 @@ if __name__ == "__main__":
                                                  "\nDefault: Use new ids from mapping file",
                         default=True, dest="use_new_ids", action='store_false')
     parser.add_argument('--ds_version', help="Data set version (is added to output path)", default="dev")
-    parser.add_argument('--pw', help="pw",)
+
     args = parser.parse_args()
 
     raw_dir = args.raw_dir
@@ -55,27 +54,10 @@ if __name__ == "__main__":
     output_dir = os.path.join(args.output_base_dir, "LHAB_" + args.ds_version + private_str, "sourcedata")
 
     ###
-    if args.participant_label:
-        old_sub_id_list = [s.strip() for s in args.participant_label]
+    if args.participant_file:
+        old_sub_id_list = read_tsv(args.participant_file)["sub_id"].tolist()
     else:
         raise Exception("No subjects specified")
-    # exclude_sub_id_list = "none"
-    # else:
-    #     # SUBJECT ID STUFF
-    #     all_sub_file = os.path.join(raw_dir, "00_PRIVATE_sub_lists/lhab_all_subjects.tsv")
-    #     exclude_sub_file = None  # os.path.join(raw_dir,
-    #     # "00_PRIVATE_sub_lists/tp5_sub_exclude.tsv")
-    #
-    #     # get subject and exclude subj list
-    #     old_sub_id_list = read_tsv(all_sub_file)["sub_id"].tolist()
-    #
-    #     try:
-    #         exclude_sub_id_list = read_tsv(exclude_sub_file)["sub_id"].tolist()
-    #         for bad in exclude_sub_id_list:
-    #             old_sub_id_list.remove(bad)
-    #             print("Removed %s" % bad)
-    #     except:
-    #         exclude_sub_id_list = "none"
 
     ses_id_list = ["T1", "T2", "T3", "T4", "T5"]
     in_ses_folder = "01_noIF"
@@ -83,21 +65,10 @@ if __name__ == "__main__":
     demo_file = os.path.join(raw_dir, "00_PRIVATE_sub_lists/dob.zip")
 
     #
-    pwd = args.pw
+    pwd = getpass.getpass("Enter the Password for dob file:")
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-
-        # # get subject and exclude subj list
-        # old_sub_id_list = read_tsv(all_sub_file)["sub_id"].tolist()
-        #
-        # try:
-        #     exclude_sub_id_list = read_tsv(exclude_sub_file)["sub_id"].tolist()
-        #     for bad in exclude_sub_id_list:
-        #         old_sub_id_list.remove(bad)
-        #         print("Removed %s" % bad)
-        # except:
-        exclude_sub_id_list = "none"
 
     calc_demos(old_sub_id_list,
                ses_id_list,
@@ -108,6 +79,7 @@ if __name__ == "__main__":
                pwd,
                use_new_ids=use_new_ids,
                new_id_lut_file=new_id_lut_file,
+               public_output=public_output,
                )
 
     print("\n\n\n\nDONE.\nConverted %d subjects." % len(old_sub_id_list))
